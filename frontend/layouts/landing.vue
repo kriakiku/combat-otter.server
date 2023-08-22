747ef0fa26fb52df77c3520a69f8bd0c7ead7606
Jonas Otter <jonas.otter@winfinity.live>
2023-08-22 16:08:37 +0600

    feat: first commit

---
+++ frontend/layouts/landing.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    GGWP
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style lang="scss" module>
+:root {
+  font-family: Roboto Slab;
+  background-color: black;
+  color: #fff;
+}
+</style>
---
+++ frontend/pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Welcome to the homepage</h1>
+    This is an auto-imported component
+  </div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  layout: "landing",
+});
+</script>
+
+<style lang="scss">
+
+</style>
